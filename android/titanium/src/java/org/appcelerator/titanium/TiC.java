fb9290d70901835001937398d5a4ae4bfa1cf90a
Manoj Kumar <mano.mykingdom@gmail.com>
2015-04-21 14:04:22 +0530

    android enhancements 1. ActionBar support for backgroundColor, titleProperties, subtitleProperties, customView, displayShowCustomEnabled 2. Window support for barColor, statusBarColor, titleAttributes 3. BaseActivity 	a. map barColor and titleAttributes to action bar as soon title is set         b. set statuBarColor on lollipop and later 4. Tic - added properties for statusBarColor and titleAttributes

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -2461,6 +2461,11 @@ public class TiC
 	* @module.api
 	*/
 	public static final String PROPERTY_SPLIT_ACTIONBAR = "splitActionBar";
+	
+	/**
+	* @module.api
+	*/
+	public static final String PROPERTY_STATUS_BAR_COLOR = "statusBarColor";
 
 	/**
 	 * @module.api
@@ -2606,6 +2611,11 @@ public class TiC
 	 * @module.api
 	 */
 	public static final String PROPERTY_TITLE = "title";
+	
+	/**
+	 * @module.api
+	 */
+	public static final String PROPERTY_TITLE_ATTRIBUTES = "titleAttributes";
 
 	/**
 	 * @module.api
