e7dbe997a695023c3d6c00927a4b29e250ae2954
Manoj Kumar <mano.mykingdom@gmail.com>
2015-04-23 17:57:21 +0530

    Android enhancements and fixes 1. Button - handle setSelectedColor & getSelectedColor 2. TimePickerDialog - support minuteInterval and cancelable 3. DatePickerDialog - support cancelable 4. TiBaseActivity - remove variable LOLLIPOP_OR_GREATER and used inline query Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP

--- android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
@@ -80,7 +80,6 @@ public abstract class TiBaseActivity extends AppCompatActivity
 	private static OrientationChangedListener orientationChangedListener = null;
 	private static OrientationEventListener orientationListener;
 
-	private static final boolean LOLLIPOP_OR_GREATER = (Build.VERSION.SDK_INT >= 21);
 	private boolean onDestroyFired = false;
 	private int originalOrientationMode = -1;
 	private boolean inForeground = false; // Indicates whether this activity is in foreground or not.
@@ -609,7 +608,7 @@ public abstract class TiBaseActivity extends AppCompatActivity
 		}
 		
 		//set status bar color
-		if (LOLLIPOP_OR_GREATER) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			String color = getIntentString(TiC.PROPERTY_STATUS_BAR_COLOR, "");
 			if(color != ""){
 				getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
