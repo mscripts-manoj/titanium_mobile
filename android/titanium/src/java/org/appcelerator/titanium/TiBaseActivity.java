eb6da0ece9139ced2152f9234ae5236bf56b6805
Manoj Kumar <mano.mykingdom@gmail.com>
2015-05-06 18:37:56 +0530

    behaviour change
    - by default action bar displayHomeAsUp will be set to true when exitOnClose of window is set to false or undefined.

--- android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiBaseActivity.java
@@ -425,6 +425,14 @@ public abstract class TiBaseActivity extends AppCompatActivity
 				}
 			});
 		}
+        
+        if (shouldFinishRootActivity() == false) {
+            runOnUiThread(new Runnable(){
+                public void run() {
+                    activityProxy.getActionBar().setDisplayHomeAsUp(true);
+                }
+            });
+        }
 	}
 
 	// Subclasses can override to provide a custom layout
